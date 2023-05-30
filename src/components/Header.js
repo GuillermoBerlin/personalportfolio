045aeedc03d36e4dc9c5460fef10e1991cf7447b
WernerBudtke <68172417+WernerBudtke@users.noreply.github.com>
2023-05-30 19:41:29 -0300

    Mobile-view changes related to small resolutions and styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import EmailIcon from '@mui/icons-material/Email';
 import GitHubIcon from '@mui/icons-material/GitHub';
+import './Header.css';
 
 export default function Header() {
   
@@ -19,7 +20,6 @@ export default function Header() {
     mainDiv: {
       marginTop: '100px',
       borderRadius: 'solid 1px red',
-      marginLeft: '220px',
     },
     subHeader: {
       fontWeight: 200,
@@ -32,27 +32,26 @@ export default function Header() {
     },
     icon: {
       fontSize: '30px',
-      marginRight: '17px',
       color: 'black', // Establece el color en negro
       cursor: 'pointer',
     },
   };
 
   return (
-    <div style={styles.mainDiv}>
+    <div style={styles.mainDiv} className='text-center text-lg-start main-div'>
       <h3>Hi, my name is</h3>
-      <h1>Guillermo Lescano</h1>
-      <h1>I'm a Full Stack Developer.</h1>
+      <h1 className='blue-color'>Guillermo Lescano</h1>
+      <h1 className='blue-color'>I'm a Full Stack Developer.</h1>
       <p style={styles.subHeader}>I´m a software developer based in Berlin, Germany.</p>
       <p style={styles.subHeader}>Currently, I’m focused on building responsive web and mobile applications.</p>
-      <div style={styles.icons}>
-        <a href="https://www.linkedin.com/in/guillermo-lescano/" target="_blank">
+      <div style={styles.icons} className='icons-container'>
+        <a href="https://www.linkedin.com/in/guillermo-lescano/" target="_blank" rel='noreferrer' className='linkedin-icon icon'>
           <LinkedInIcon style={{ ...styles.icon, color: 'black' }} />
         </a>
-        <a href="https://github.com/GuillermoBerlin" target="_blank">
+        <a href="https://github.com/GuillermoBerlin" target="_blank" rel='noreferrer' className='github-icon icon'>
           <GitHubIcon style={{ ...styles.icon, color: 'black' }} />
         </a>
-        <EmailIcon style={styles.icon} onClick={handleEmailClick} />
+        <EmailIcon style={styles.icon} onClick={handleEmailClick} className='email-icon icon'/>
       </div>
     </div>
   );
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,21 +1,16 @@
 import React, { useState } from 'react';
 import { Row, Col, Image, Button, Badge } from 'react-bootstrap';
 import GitHubIcon from '@mui/icons-material/GitHub';
-
+import useWindowSize from './useWindowSize';
+import './Project.css';
 const Project = () => {
   
   const [hovered, setHovered] = useState(false);
+  const windowSize = useWindowSize();
 
   const styles = {
     img: {
-      width: "400px",
-      height: "300px",
-      filter: hovered ? "blur(2px)" : "none",
-    },
-    imgContainer: {
-      position: "relative",
-      display: "inline-block",
-      textAlign: "right",
+      filter: hovered && windowSize.width > 700 ? "blur(2px)" : "none",
     },
     buttonContainer: {
       position: "absolute",
@@ -23,14 +18,21 @@ const Project = () => {
       left: "50%",
       transform: "translate(-50%, -50%)", 
     },
+    mobileButtonContainer: {
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      marginTop: "20px",
+    },
     colLeft: {
       display: "flex",
       flexDirection: "row",
-      justifyContent: "flex-end",
-      marginRight: "20px",
+      justifyContent: "flex-start",
+      // marginRight: "20px",
     },
     colRight: {
-      marginLeft: "20px",
+      // marginLeft: "20px",
+      // textAlign: 'left',
     },
     button: {
       display: hovered ? "block" : "none",
@@ -39,12 +41,23 @@ const Project = () => {
       borderRadius: '50px',
       border: "none",
     },
+    mobileButton:{
+      margin: "12px",
+      width: "160px",
+      borderRadius: '50px',
+      border: "none",
+    },
     pills: {
       display: 'flex',
       gap: "10px",
+      width: "400px"
+    },
+    title:{
+      width: "400px",
     },
     paragraph: {
       lineHeight: "1.5",
+      width: "400px",
       maxWidth: "400px",
       marginTop: "15px",
       marginBottom: "15px",
@@ -67,27 +80,32 @@ const Project = () => {
 
   return (
     <Row style={styles.row} className="d-flex align-items-center">
-      <Col  style={styles.colLeft}>
+      <Col  style={styles.colLeft} className='justify-content-center justify-content-lg-start'>
 
-      <div style={styles.imgContainer}
+      <div
            onMouseEnter={() => setHovered(true)}
            onMouseLeave={() => setHovered(false)} 
             >
 
         <Image 
           style={styles.img} 
-          src="https://wpleaders.com/wp-content/uploads/2021/05/WPBeginner.png"  
+          src="https://images.unsplash.com/photo-1543373014-cfe4f4bc1cdf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aGlnaCUyMHJlc29sdXRpb258ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60"  
         />
         
+        {windowSize.width < 700 ? <div style={styles.mobileButtonContainer}>
+          <Button variant="outline-dark" className="btn-light" style={styles.mobileButton}>Visit Website</Button>
+          <Button variant="outline-dark" className="btn-light" style={styles.mobileButton}><div style={styles.seeTheCode}>See the code <GitHubIcon fontSize="small"/></div></Button>
+        </div> :
         <div style={styles.buttonContainer} onMouseEnter={() => setHovered(true)}> 
-        <Button variant="outline-dark" className="btn-light" style={styles.button}>Visit Website</Button>
-        <Button variant="outline-dark" className="btn-light" style={styles.button}><div style={styles.seeTheCode}>See the code <GitHubIcon fontSize="small"/></div></Button>
-        </div>
+          <Button variant="outline-dark" className="btn-light" style={styles.button}>Visit Website</Button>
+          <Button variant="outline-dark" className="btn-light" style={styles.button}><div style={styles.seeTheCode}>See the code <GitHubIcon fontSize="small"/></div></Button>
+        </div>}
+
       </div>
         
       </Col>
-      <Col style={styles.colRight}>
-        <h4>Dopefolio</h4>
+      <Col style={styles.colRight} className='d-flex flex-column  p-5 pt-0 pb-0 p-md-4 mt-4 mt-md-0 align-items-center align-items-lg-start'>
+        <h4 style={styles.title} className='blue-color'>Dopefolio</h4>
         <p style={styles.paragraph}>Dopefolio is a successful Open-Source project that I created which have been featured on some of the biggest tech sites like CSS-Tricks, Hostinger, etc & used by thousands of developers globally
         </p>
         <div style={styles.pills}>
